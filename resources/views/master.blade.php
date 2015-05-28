4488c0212cbf1ff297e938d653d25f38e2f10a53
Marco Bellan <marcobellan97@gmail.com>
2015-05-28 12:55:31 +0000

    Deleted .idea, adjusted gitignore, fix issue #12

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+    <link rel="stylesheet" href="css/materialize.min.css">
     <!-- Jquery -->
-    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="js/jquery-2.1.4.min.js"></script>
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+    <script src="js/materialize.min.js"></script>
 </head>
 <body>
     <div class="container">

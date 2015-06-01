81070a2e1a8bad0c6ccdb9d6c8e868978d8f3e3a
Marco Bellan <marcobellan97@gmail.com>
2015-06-01 08:43:42 +0000

    New communication standard

--- utils/controller.py
+++ utils/controller.py
@@ -19,7 +19,7 @@ def main_loop():
 	#update db
 	fetch_url(base_url+"completed/"+str(data["id"]))
 	#reset position
-	serial.write("x|x")
+	serial.write("GoHome")
 	wait_answer()
 def fetch_url(url):
 	global data

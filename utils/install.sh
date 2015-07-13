0028f8b30395fe5021b44f01feaae4af4c039b3d
Marco Bellan <marcobellan97@gmail.com>
2015-07-13 15:54:40 +0000

    install.sh changed name

--- utils/install.sh
+++ utils/install.sh
@@ -18,10 +18,10 @@ sudo apt-get install avahi-daemon -y > /dev/null
 
 echo "Changing host"
 sudo sed -i '$ d' /etc/hosts
-sudo echo "127.0.1.1	drink-maker" >> /etc/hosts
+sudo echo "127.0.1.1	drink" >> /etc/hosts
 
 echo "Changing hostname"
-sudo echo "drink-maker" > /etc/hostname
+sudo echo "drink" > /etc/hostname
 
 echo "Creating usergroup www-data"
 sudo useradd www-data 

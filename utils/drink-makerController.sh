b4922c4aefefb2ddbbda8471ebb8e8eb33612ffa
Marco Bellan <marcobellan97@gmail.com>
2015-08-07 15:33:36 +0000

    drink-makerController.sh final update system

--- utils/drink-makerController.sh
+++ utils/drink-makerController.sh
@@ -9,6 +9,27 @@ case "$1" in
     if [[ "$OUTPUT" == *"files changed"* ]]
     then
       echo "Updates found"
+      if [[ "$OUTPUT" == *"install/default"* ]]
+      then
+        echo "Copying nginx updated files"
+        sudo cp /var/www/drink-maker/install/default /etc/nginx/sites-available/default
+        sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+      fi
+      if [[ "$OUTPUT" == *"install/www.conf"* ]]
+      then
+        echo "Updating www.conf"
+        sudo rm /etc/php5/fpm/pool.d/www.conf
+        sudo cp /var/www/install/www.conf /etc/php5/fpm/pool.d/www.conf
+      fi
+      if [[ "$OUTPUT" == *"install/php.ini"* ]]
+      then
+        echo "Updating php.ini"
+        sudo rm /etc/php5/fpm/php.ini
+        sudo cp /var/www/install/php.ini /etc/php5/fpm/php.ini
+      fi
+      echo "Restarting services"
+      sudo service php5-fpm restart
+      sudo service nginx restart
     elif [[ "$OUTPUT" == *"Already up-to-date."* ]]
     then
       echo "Already up-to-date."

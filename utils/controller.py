8a1b29617b9206834f3d572a99cf651491d645a3
Marco Bellan <marcobellan97@gmail.com>
2015-08-24 13:21:08 +0000

    controller.py edited online with Bitbucket

--- utils/controller.py
+++ utils/controller.py
@@ -81,7 +81,7 @@ def write_data(data):
 def connect_wifi(ssid,password):
 	os.system("sudo bash /var/www/drink-maker/utils/connect.sh "+ssid + " " + password)
 
-os.system("ArduLoad /var/www/drink-maker/utils/drink-maker.cpp.hex")
+os.system("bash /home/pi/bin/ArduLoad /var/www/drink-maker/utils/drink-maker.cpp.hex")
 
 time.sleep(3)
 base_url="http://drink/"

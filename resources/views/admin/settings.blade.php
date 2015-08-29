531c0e25df29b36f5b71c31ba17ad6cc592e814b
Marco Bellan <marcobellan97@gmail.com>
2015-08-29 15:56:38 +0000

    HTML refactor

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -2,95 +2,73 @@
     <form id="form" action="{{url('configure')}}" method="POST">
         <input type="hidden" id="ssid" name="ssid" value="{{$settings["wifi"]}}">
         <div class="row">
-            <div class="col s6">
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="text" value="{{$settings['username']}}" name="username" id="username" >
-                        <label for="username">Username</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="password" name="new_psw" id="new_psw" >
-                        <label for="new_psw">New password</label>
-                    </div>
-                </div>
+            <div class="col offset-s1 s5 input-field">
+                <input type="text" value="{{$settings['username']}}" name="username" id="username" >
+                <label for="username">Username</label>
             </div>
-            <div class="col s6">
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="password" name="old_psw" id="old_psw" >
-                        <label for="old_psw">Old password</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="password" name="re_new_psw" id="re_new_psw" >
-                        <label for="re_new_psw">Repeat new password</label>
-                    </div>
-                </div>
+            <div class="col offset-s1 s5 input-field">
+                <input type="password" name="old_psw" id="old_psw" >
+                <label for="old_psw">Old password</label>
             </div>
         </div>
         <div class="row">
-            <div class="col s6">
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="number" min="10" value="{{$settings['volume']}}" max="32000" name="volume" id="volume" >
-                        <label for="volume">Drink max volume(cL)</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
-                        <input type="number" min="15" value="{{$settings['timeout_time']}}" max="3600" name="timeout_time" id="timeout_time" >
-                        <label for="timeout_time">Timeout time(seconds)</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s10 offset-s1">
-                        <div class="switch" >
-                            <label>Enable lights: </label>
-                            <label>
-                                Off
-                                <input type="checkbox" {{$settings['has_lights']?'checked':'unchecked'}} name="has_lights" id="has_lights" >
-                                <span class="lever"></span>
-                                On
-                            </label>
-                        </div>
-                    </div>
+            <div class="col offset-s1 s5 input-field">
+                <input type="password" name="new_psw" id="new_psw" >
+                <label for="new_psw">New password</label>
+            </div>
+            <div class="col offset-s1 s5 input-field">
+                <input type="password" name="re_new_psw" id="re_new_psw" >
+                <label for="re_new_psw">Repeat new password</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col offset-s1 s5 input-field">
+                <input type="number" min="10" value="{{$settings['volume']}}" max="32000" name="volume" id="volume" >
+                <label for="volume">Drink max volume(cL)</label>
+            </div>
+            <div class="col offset-s1 s5 input-field">
+                <input type="number" min="15" value="{{$settings['timeout_time']}}" max="3600" name="timeout_time" id="timeout_time" >
+                <label for="timeout_time">Timeout time(seconds)</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col offset-s1 s5 ">
+                <label>Start method</label>
+                <select name="start_method" class="browser-default">
+                    <option value="0">Auto</option>
+                    <option value="1">Button</option>
+                    <option value="2">Coin</option>
+                </select>
+            </div>
+            <div class="col offset-s1 s5 ">
+                <label>Default order status</label>
+                <select name="initial_status" class="browser-default">
+                    <option value="0">To be approved</option>
+                    <option value="1">Approved</option>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col offset-s1 s5">
+                <div class="switch">
+                    <label>Enable lights: </label>
+                    <label>
+                        Off
+                        <input type="checkbox" {{$settings['has_lights']?'checked':'unchecked'}} name="has_lights" id="has_lights" >
+                        <span class="lever"></span>
+                        On
+                    </label>
                 </div>
             </div>
-            <div class="col s6">
-                <div class="row">
-                    <div class="col s10 offset-s1">
-                        <label>Start method</label>
-                        <select name="start_method" class="browser-default">
-                            <option value="0">Auto</option>
-                            <option value="1">Button</option>
-                            <option value="2">Coin</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s10 offset-s1">
-                        <label>Default order status</label>
-                        <select name="initial_status" class="browser-default">
-                            <option value="0">To be approved</option>
-                            <option value="1">Approved</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s10 offset-s1">
-                        <div class="switch" >
-                            <label>Play notification sounds: </label>
-                            <label>
-                                Off
-                                <input type="checkbox" {{$settings['play_sounds']?'checked':'unchecked'}} name="play_sounds" id="play_sounds" >
-                                <span class="lever"></span>
-                                On
-                            </label>
-                        </div>
-                    </div>
+            <div class="col offset-s1 s5 ">
+                <div class="switch">
+                <label>Play notification sounds: </label>
+                    <label>
+                        Off
+                        <input type="checkbox" {{$settings['play_sounds']?'checked':'unchecked'}} name="play_sounds" id="play_sounds" >
+                        <span class="lever"></span>
+                        On
+                    </label>
                 </div>
             </div>
         </div>
@@ -145,7 +123,7 @@
             </div>
         </div>
         <div class="modal-footer">
-            <a href="" onclick="$('#warner').show()" class=" modal-dismisser waves-effect waves-green btn-flat">Connect</a>
-            <a href="" onclick="$('#password').val('');" class=" modal-dismisser waves-effect waves-green btn-flat">Cancel</a>
+            <a href="#" onclick="$('#warner').show()" class=" modal-dismisser waves-effect waves-green btn-flat">Connect</a>
+            <a href="#" onclick="$('#password').val('');" class=" modal-dismisser waves-effect waves-green btn-flat">Cancel</a>
         </div>
     </div>

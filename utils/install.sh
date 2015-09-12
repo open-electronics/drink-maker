9c8dd2b080a92f1d2af3a6bf792412fb5fec0e49
MarcoROG <mark.sg97@gmail.com>
2015-09-12 16:15:57 +0200

    install.sh now calls setToken

--- utils/install.sh
+++ utils/install.sh
@@ -124,7 +124,7 @@ sudo chmod +x /etc/init.d/drink-maker.sh
 sudo update-rc.d drink-maker.sh defaults
 
 echo "Saving the access token"
-sudo sed -i "s/YOURGITHUBPERSONALACCESSTOKENHERE/$1/g" /var/www/drink-maker/composer.json
+sudo bash /var/www/drink-maker/utils/setToken.sh $1
 
 echo "Installing RandA"
 cp /var/www/drink-maker/utils/install/RandAinstall.sh /home/pi/RandAinstall.sh
